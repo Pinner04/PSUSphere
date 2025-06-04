7dd16ed92d3d92de8fcb910b33c7abe0165ca7b5
Pinner Sama <jadeensomo7@gmail.com>
2025-06-05 04:59:34 +0800

    hm

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home' # This will pass the list of organizations as 'home' to home.html
+    template_name = "home.html"
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization' # This will pass the list of organizations as 'organization' to org_list.html
+    template_name = 'org_list.html'
+    paginate_by = 5
