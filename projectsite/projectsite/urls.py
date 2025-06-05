ef51f202a4429feec7c8d6519bd8198a3fcae2d3
Andrew <23andrew99@gmail.com>
2025-06-05 11:47:44 +0800

    applied crud

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -15,15 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
-    path('organization_list', OrganizationList.as_view(), name='organization-list'),
-    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
-    path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<int:pk>/',OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),
+
+    path('org_members/', views.OrgMemberList.as_view(), name='orgmember-list'),
+    path('org_members/add/', views.OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('org_members/<int:pk>/', views.OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('org_members/<int:pk>/delete/', views.OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+
+    path('students/', views.StudentList.as_view(), name='student-list'),
+    path('students/add/', views.StudentCreateView.as_view(), name='student-add'),
+    path('students/<int:pk>/', views.StudentUpdateView.as_view(), name='student-update'),
+    path('students/<int:pk>/delete/', views.StudentDeleteView.as_view(), name='student-delete'),
+
+    path('colleges/', views.CollegeList.as_view(), name='college-list'),
+    path('colleges/add/', views.CollegeCreateView.as_view(), name='college-add'),
+    path('colleges/<int:pk>/', views.CollegeUpdateView.as_view(), name='college-update'),
+    path('colleges/<int:pk>/delete/', views.CollegeDeleteView.as_view(), name='college-delete'),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
+from studentorg.models import Organization, OrgMember, Student, College
+from studentorg.forms import OrganizationForm, OrgMemberForm, StudentForm, CollegeForm
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 class HomePageView(ListView):
     model = Organization
@@ -15,6 +16,17 @@ class OrganizationList(ListView):
     context_object_name = 'organization' # This will pass the list of organizations as 'organization' to org_list.html
     template_name = 'org_list.html'
     paginate_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+        if query:
+            return qs.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query) |
+                Q(college__college_name__icontains=query) # Assuming you want to search by college name
+            )
+        return qs
 
 class OrganizationCreateView(CreateView):
     model = Organization
@@ -32,3 +44,104 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+class OrgMemberList(ListView):
+    model = OrgMember
+    context_object_name = 'orgmember_list'
+    template_name = 'orgmember_list.html' # You'll need to create this template
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+        if query:
+            # Search by student's last name, first name, or organization name
+            return qs.filter(
+                Q(student__lastname__icontains=query) |
+                Q(student__firstname__icontains=query) |
+                Q(organization__name__icontains=query)
+            )
+        return qs
+
+class OrgMemberCreateView(CreateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_add.html' # You'll need to create this template
+    success_url = reverse_lazy('orgmember-list') # Assumes you'll name this URL pattern 'orgmember-list'
+
+class OrgMemberUpdateView(UpdateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_edit.html' # You'll need to create this template
+    success_url = reverse_lazy('orgmember-list')
+
+class OrgMemberDeleteView(DeleteView):
+    model = OrgMember
+    template_name = 'orgmember_del.html' # You'll need to create this template
+    success_url = reverse_lazy('orgmember-list')
+
+class StudentList(ListView):
+    model = Student
+    context_object_name = 'student_list'
+    template_name = 'student_list.html'
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+        if query:
+            # Search by student ID, lastname, firstname, or program name
+            return qs.filter(
+                Q(student_id__icontains=query) |
+                Q(lastname__icontains=query) |
+                Q(firstname__icontains=query) |
+                Q(program__prog_name__icontains=query)
+            )
+        return qs
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student_add.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student_edit.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'student_del.html'
+    success_url = reverse_lazy('student-list')
+
+class CollegeList(ListView):
+    model = College
+    context_object_name = 'college_list'
+    template_name = 'college_list.html'
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+        if query:
+            return qs.filter(college_name__icontains=query)
+        return qs
+
+class CollegeCreateView(CreateView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college_add.html'
+    success_url = reverse_lazy('college-list')
+
+class CollegeUpdateView(UpdateView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college_edit.html'
+    success_url = reverse_lazy('college-list')
+
+class CollegeDeleteView(DeleteView):
+    model = College
+    template_name = 'college_del.html'
+    success_url = reverse_lazy('college-list')
