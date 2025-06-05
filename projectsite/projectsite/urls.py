4093ac361893a9d55f4e085235a7060339433a60
Andrew <23andrew99@gmail.com>
2025-06-05 12:20:57 +0800

    added login logout

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list/', OrganizationList.as_view(), name='organization-list'),
